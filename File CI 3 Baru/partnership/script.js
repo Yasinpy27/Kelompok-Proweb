246bfafefa6f63287ab8a5705a9a9a95b05caa6c
Yasinpy27 <120181104+Yasinpy27@users.noreply.github.com>
2023-12-28 20:21:57 +0700

    Create script.js

---
+++ File CI 3 Baru/partnership/script.js
@@ -0,0 +1,8 @@
+// Add your custom JavaScript code here
+document.getElementById('feedbackForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Add logic to handle form submission (e.g., sending feedback to the server)
+    // You can use AJAX or other methods as per your requirements
+  });
+  
+  
